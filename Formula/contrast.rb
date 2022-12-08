65268d7024affb0c64db2565f9e27eca85ce1ec1
Andrew Shanks <andrew.shanks@contrastsecurity.com>
2022-12-08 10:07:06 +0000

    update to version 1.0.21
    update to version 1.0.21

--- Formula/contrast.rb
+++ Formula/contrast.rb
@@ -5,13 +5,13 @@ class Contrast < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://pkg.contrastsecurity.com/artifactory/cli/1.0.20/mac/contrast"
-    sha256 "911ae77717d808ef573866e92dbd1c8dfd3e2f50b06720f26745f68278240435"
+    url "https://pkg.contrastsecurity.com/artifactory/cli/1.0.21/mac/contrast"
+    sha256 "8e29362e4d8e998763479561d08c727a43d366cb62e9200454683ff52cdf02d0"
 
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://pkg.contrastsecurity.com/artifactory/cli/1.0.20/linux/contrast"
-      sha256 "01ce07452a09856e641e4fedb86692aa843c32a4590900a0b0b4fc82e3360b75"
+      url "https://pkg.contrastsecurity.com/artifactory/cli/1.0.21/linux/contrast"
+      sha256 "27e12342b796dd3452e1c30cbbeb6b5b142b6b31dc3cda38c7e1976d9c0231dc"
     end
   end
 
