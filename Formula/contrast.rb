bb57bf799067925a394d4c4ba1302ac296d95309
Andrew Shanks <andrew.shanks@contrastsecurity.com>
2023-09-12 09:55:13 +0100

    Update contrast.rb
    version 2.1.2

--- Formula/contrast.rb
+++ Formula/contrast.rb
@@ -5,13 +5,13 @@ class Contrast < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://pkg.contrastsecurity.com/artifactory/cli/v2/2.1.0/mac/contrast"
-    sha256 "b7f175d240fbaa7866b58db90557da050ba895e04084a260f8b28b9b010903d9"
+    url "https://pkg.contrastsecurity.com/artifactory/cli/v2/2.1.2/mac/contrast"
+    sha256 "63018bf30659d3ee3484ebacb7b42c7f04330589b7bde1f05d29e6ead209d51f"
 
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://pkg.contrastsecurity.com/artifactory/cli/v2/2.1.0/linux/contrast"
-      sha256 "51c7581269a964488e4bc8200f843d8b83341f10d80860936bca42baa91b67a4"
+      url "https://pkg.contrastsecurity.com/artifactory/cli/v2/2.1.2/linux/contrast"
+      sha256 "17a834ee76b9683fdf02a91615192d0b585411b43ff171a13b1caf1056ac66d8"
     end
   end
 
