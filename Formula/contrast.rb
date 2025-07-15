bbfe28a907a0fe28c7a8a4c0e7261a89a8522061
christopher.dunne <christopher.dunne@contrastsecurity.com>
2025-07-15 15:49:20 +0100

    v3.2.2

--- Formula/contrast.rb
+++ Formula/contrast.rb
@@ -5,13 +5,13 @@ class Contrast < Formula
   license "MIT"
 
   if OS.mac?
-    url "https://contrastsecurity.jfrog.io/artifactory/cli/v2/3.2.1/mac/contrast"
-    sha256 "10bcde9f7719230d1dd15ae2d199e59b383b7e3a3aedbd2eee312e4833cccc73"
+    url "https://contrastsecurity.jfrog.io/artifactory/cli/v2/3.2.2/mac/contrast"
+    sha256 "e0f992cd9028640686100d14fc3451224ec4255866bcb10d2dfce639c0cd3642"
 
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://contrastsecurity.jfrog.io/artifactory/cli/v2/3.2.1/linux/contrast"
-      sha256 "9b6e3d9471b82253a6b8dfedfcdd342160d636c7257a7fb7f3ea265d1b416b83"
+      url "https://contrastsecurity.jfrog.io/artifactory/cli/v2/3.2.2/linux/contrast"
+      sha256 "6c3740a6ea1b02279da2b36366cd1077e3af5c329c849e3e61231326d2aabae0"
     end
   end
 
